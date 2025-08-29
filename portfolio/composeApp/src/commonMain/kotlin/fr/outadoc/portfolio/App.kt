8e3aefad275466bd5509cb9f7808d94ae6376cdd
Baptiste Candellier <baptiste@candellier.me>
2025-08-29 19:53:47 +0200

    add all buttons

--- portfolio/composeApp/src/commonMain/kotlin/fr/outadoc/portfolio/App.kt
+++ portfolio/composeApp/src/commonMain/kotlin/fr/outadoc/portfolio/App.kt
@@ -2,6 +2,7 @@ package fr.outadoc.portfolio
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,30 +11,50 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import portfolio.composeapp.generated.resources.Res
 import portfolio.composeapp.generated.resources.avatar
 import portfolio.composeapp.generated.resources.avatar_caption
+import portfolio.composeapp.generated.resources.bluesky
+import portfolio.composeapp.generated.resources.envelope
+import portfolio.composeapp.generated.resources.github
+import portfolio.composeapp.generated.resources.linkedin
+import portfolio.composeapp.generated.resources.mastodon
+import portfolio.composeapp.generated.resources.rss
 
 @Composable
 fun App() {
-    MaterialTheme {
+    MaterialTheme(
+        colorScheme = darkColorScheme()
+    ) {
         Scaffold { insets ->
             Row(
                 modifier = Modifier
                     .padding(insets)
-                    .background(MaterialTheme.colorScheme.primaryContainer)
+                    .background(
+                        Brush.linearGradient(
+                            colors = listOf(
+                                Color(0xffb455d0),
+                                Color(0xff555bca)
+                            ),
+                        )
+                    )
                     .fillMaxSize(),
                 verticalAlignment = Alignment.CenterVertically,
             ) {
@@ -60,11 +81,67 @@ fun App() {
                         style = MaterialTheme.typography.titleMedium,
                     )
 
-                    Button(onClick = { uriHandler.openUri("https://mastodon.social/@outadoc") }) {
-                        Text("Mastodon")
-                    }
+                    SocialButton(
+                        title = "Mastodon",
+                        url = "https://mastodon.social/@outadoc",
+                        icon = Res.drawable.mastodon
+                    )
+
+                    SocialButton(
+                        title = "Bluesky",
+                        url = "https://bsky.app/profile/outadoc.fr",
+                        icon = Res.drawable.bluesky
+                    )
+
+                    SocialButton(
+                        title = "GitHub",
+                        url = "https://github.com/outadoc",
+                        icon = Res.drawable.github
+                    )
+
+                    SocialButton(
+                        title = "LinkedIn",
+                        url = "https://www.linkedin.com/in/candellierba",
+                        icon = Res.drawable.linkedin
+                    )
+
+                    SocialButton(
+                        title = "Blog",
+                        url = "https://blog.outadoc.fr",
+                        icon = Res.drawable.rss
+                    )
+
+                    SocialButton(
+                        title = "Email",
+                        url = "mailto:baptiste@candellier.me",
+                        icon = Res.drawable.envelope
+                    )
                 }
             }
+        }
+    }
+}
+
+@Composable
+private fun SocialButton(
+    title: String,
+    url: String,
+    icon: DrawableResource,
+) {
+    val uriHandler = LocalUriHandler.current
+    Button(
+        onClick = { uriHandler.openUri(url) },
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Icon(
+                modifier = Modifier.size(16.dp),
+                painter = painterResource(icon),
+                contentDescription = null
+            )
+            Text(title)
         }
     }
 }
--- portfolio/composeApp/src/jvmMain/kotlin/fr/outadoc/portfolio/main.kt
+++ portfolio/composeApp/src/jvmMain/kotlin/fr/outadoc/portfolio/main.kt
@@ -1,9 +1,15 @@
 package fr.outadoc.portfolio
 
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.singleWindowApplication
 
 fun main() {
-    singleWindowApplication {
+    singleWindowApplication(
+        title = "Portfolio",
+        state = WindowState(size = DpSize(1500.dp, 1800.dp))
+    ) {
         App()
     }
 }
