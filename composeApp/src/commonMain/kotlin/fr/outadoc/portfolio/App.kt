ec2e5667a3102f08c6aec9f22772ed9fe0273663
Baptiste Candellier <baptiste@candellier.me>
2025-08-30 16:37:49 +0200

    make it squared

--- composeApp/src/commonMain/kotlin/fr/outadoc/portfolio/App.kt
+++ composeApp/src/commonMain/kotlin/fr/outadoc/portfolio/App.kt
@@ -1,6 +1,5 @@
 package fr.outadoc.portfolio
 
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -9,13 +8,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -29,11 +29,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.PointerIcon
@@ -94,11 +94,12 @@ fun App() {
                             Image(
                                 modifier = Modifier
                                     .hoverCard()
-                                    .size(160.dp)
-                                    .clip(CircleShape),
+                                    .size(160.dp),
                                 painter = painterResource(Res.drawable.avatar),
                                 contentDescription = stringResource(Res.string.avatar_caption),
                             )
+
+                            Spacer(modifier = Modifier.height(8.dp))
 
                             Text(
                                 "Baptiste Candellier",
@@ -182,6 +183,7 @@ fun App() {
 fun Modifier.hoverCard(
     factor: Float = 20f,
     elevation: Dp = 8.dp,
+    shape: Shape = RoundedCornerShape(32.dp),
 ): Modifier {
     var size by remember { mutableStateOf(IntSize.Zero) }
     var offset by remember { mutableStateOf(Offset.Zero) }
@@ -219,7 +221,7 @@ fun Modifier.hoverCard(
             rotationX = -offsetY * factor
             rotationY = offsetX * factor
         }
-        .shadow(elevation, shape = CircleShape)
+        .shadow(elevation, shape = shape)
 }
 
 @Composable
